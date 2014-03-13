034b38360070f51442fb08dda2fb8118a5c8cc72
Edward Shaw <edwardshaw9@gmail.com>
2014-03-13 15:16:34 +0000

    Added cli-args, fixed bugs, cleaned up hackyness

--- ecsprint.py
+++ ecsprint.py
@@ -15,30 +15,34 @@ except AttributeError:
     quote = pipes.quote
 
 parser = argparse.ArgumentParser(description="Print on ECS printers")
-parser.add_argument('-p', metavar="PRINTER", default="mfp0hdup", help="Printer name. Eg mfp0 or tp1hdup")
+parser.add_argument('-P', metavar="PRINTER", default="mfp0hdup", help="Printer name. Eg mfp0 or tp1hdup")
 parser.add_argument('host', default="ecs.ox.ac.uk", help="Host. Eg m10es@ecs.ox.ac.uk")
-#parser.add_argument('file',  help="Filename")
-parser.add_argument('files', default=[], nargs="+", help="Filenames")
+parser.add_argument('files', default=[], nargs="+", help="File-names of files to be printed")
+parser.add_argument('-c', metavar="COMMAND", default="lpr", help="Print command.")
+parser.add_argument('--dryrun', action='store_true', help="Do not print.")
+parser.add_argument('-args', help="Print args.")
+parser.add_argument('-v', action="store_true", help="Verbose.")
 
 args = parser.parse_args()
-files = list(map(quote, args.files))
+files = args.files
 
-print("Files: " + str(files))
+if args.v:
+    print("Files: " + ", ".join(files))
 
 # eg m10es@ecs.ox.ac.uk
 userhost = args.host.split('@', 1)
 if len(userhost)==1:
     user=None
-    host=userhost.strip()
+    host=userhost[1].strip()
 else:
     user = userhost[0].strip()
     host = (userhost[1] or "ecs.ox.ac.uk").strip()
 
-print(user)
-print(host)
+if args.v:
+    print("Connecting to %s@%s" % (user, host))
 
 # Printer name. Default is mfp0 horizontal duplex. This is near the ug social area. You might want tp1hdup
-printer = args.p
+printer = args.P
 
 ssh = paramiko.SSHClient()
 ssh.load_system_host_keys()
@@ -52,21 +56,21 @@ tmpdir_proc = "mktemp -d --suff .ecsprint || mktemp -d -t .ecsprint"
 _, tmpdirOUT, tmpdirERR = ssh.exec_command(tmpdir_proc)
 tmpdir = tmpdirOUT.readlines()[0].strip() + "/"
 
-print(tmpdir)
-
 #Rsync! Whole files only. Relative mode. Set permissions so only we can read it. Output the file locations for printing.
-rsync_args = ['rsync', '-WR', '--chmod=ugo-rwx,u+rw', '--out-format=%n%L'] + files + ['%s@%s:%s' % (user, host, tmpdir)]
+rsync_args = ['rsync', '-WR', '--chmod=ugo-rwx,u+rw', '--out-format=%n'] + files + ['%s@%s:%s' % (user, host, tmpdir)]
 rsync_output = subprocess.check_output(rsync_args)
 
 # server_files lists all of the files on the server.
 # Rsync lists directories as well as files. Lets filter them out.
 server_files = list(map(lambda str: tmpdir+str , filter(lambda str: str[-1]!='/', rsync_output.decode("utf-8").split())))
 
-print_args = " ".join(["-P %s" % printer] + server_files)
-print(print_args)
-_, printout, printerr = ssh.exec_command(print_args)
-print(printout.read())
-#print(printerr.read(), file=sys.stderr)
+if not args.dryrun:
+    print_args = " ".join([args.c] + shlex.split(args.args) + ["-P %s" % printer] + server_files)
+    if args.v:
+        print(print_args)
+    _, printout, printerr = ssh.exec_command(print_args)
+    print(printout.read())
+    print(printerr.read(), file=sys.stderr)
 
 # Remove the temporary folder. I feel uneasy about this line.
 rmtmp_proc = "rm -rf " + quote(tmpdir)
